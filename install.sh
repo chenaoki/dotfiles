6f1b200fc7c2a9b271b5c5fa88c07d24be2e0d95
Naoki Tomii <naoki_tomii@bmpe.t.u-tokyo.ac.jp>
2016-01-08 18:53:47 +0900

    修正

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-find ./ -maxdepth 1 -type f -regex "\.*.*" | tee /dev/stderr | xargs -I {} cp {} ~/
+#find ./ -maxdepth 1 -type f -regex "\.*.*" | tee /dev/stderr | xargs -I {} cp {} ~/
+find ./ -maxdepth 1 -type f -regex "\.\/\..*" | tee /dev/stderr | xargs echo
