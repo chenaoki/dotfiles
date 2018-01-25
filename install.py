ea25c11b5f2bd80d798e796287944aac642cd2e1
Naoki Tomii <chenaoki@gmail.com>
2018-01-25 13:33:31 +0900

    update

--- install.py
+++ install.py
@@ -6,8 +6,6 @@ if __name__ == '__main__':
   for obj in objs:
     if not '.git' in obj and obj != './' and not 'install.py' in obj:
       cmd = 'rm -rf ~/{0}'.format(obj)
-      print cmd
       os.system(cmd)
       cmd = 'ln -s {1}/{0} ~/{0}'.format(obj, os.getcwd())
-      print cmd
       os.system(cmd)
